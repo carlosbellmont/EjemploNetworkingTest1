01b998fb244eb51284838fcc9639e53e5a0bc85a
cbellmont <carlos.bellmont@gmail.com>
2022-01-31 15:45:41 +0100

    Hot fix

--- app/src/main/java/com/example/ejemplonetworkingtest/data/PersonajeRemoteDataSource.kt
+++ app/src/main/java/com/example/ejemplonetworkingtest/data/PersonajeRemoteDataSource.kt
@@ -1,13 +1,10 @@
 package com.example.ejemplonetworkingtest.data
 
-import com.example.ejemplonetworkingtest.app.App
-import com.example.ejemplonetworkingtest.model.PersonajeModel
 import com.example.ejemplonetworkingtest.model.PersonajeDataSource
-import kotlinx.coroutines.*
+import com.example.ejemplonetworkingtest.model.PersonajeModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import kotlin.coroutines.coroutineContext
 
 class PersonajeRemoteDataSource(apiClient: ApiClient) : PersonajeDataSource {
 
@@ -19,26 +16,22 @@ class PersonajeRemoteDataSource(apiClient: ApiClient) : PersonajeDataSource {
         call = service.obtenerPersonajes()
         call?.enqueue(object : Callback<List<PersonajeModel>> {
             override fun onFailure(call: Call<List<PersonajeModel>>, t: Throwable) {
-                CoroutineScope(Dispatchers.IO).launch {
-                    callback.onError(t.message)
-                }
+                callback.onError(t.message)
             }
 
             override fun onResponse(call: Call<List<PersonajeModel>>, response: Response<List<PersonajeModel>>) {
-                CoroutineScope(Dispatchers.IO).launch {
-                    response.body()?.let {
-                        if (response.isSuccessful) {
-                            callback.onSuccess(it)
-                        } else {
-                            callback.onError("error")
-                        }
-                    }
+                if (response.isSuccessful) {
+                    callback.onSuccess(response.body())
+                } else {
+                    callback.onError("error")
                 }
-            }
-        })
-    }
 
-    override fun cancel() {
-        call?.cancel()
-    }
+            }
+
+    })
+}
+
+override fun cancel() {
+    call?.cancel()
+}
 }
